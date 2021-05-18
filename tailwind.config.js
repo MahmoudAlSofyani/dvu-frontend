01e7c1f33b7122904b9ef4862661dc4bd1907dff
Mahmoud AlSofyani <msafar95@hotmail.com>
2021-05-18 19:14:23 +0400

    tailwindcss installed and configured

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      black: "#000000",
+      red: "#de1f26",
+      darkGray: "#2f2f37",
+      white: "#f9fafb",
+      charcoal: "#1e1e24",
+      green: "#45de1f",
+      transparent: "transparent",
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
